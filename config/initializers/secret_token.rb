325a20d45381c62b32544738df08d46c03edd2dc
TSL student <test@example.com>
2013-07-31 13:18:11 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IdeaNotebook::Application.config.secret_key_base = '6edfc3a8b4bdc2b0b68290f53e5bac5d1f0bd838e6d044a683659bfd282c7499a9923a9383b50635aa0aa9f7a772a5edfba13201295371cbc37a4eacebd88be2'
